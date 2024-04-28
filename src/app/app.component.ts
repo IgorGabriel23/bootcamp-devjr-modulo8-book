9a1a453dcf2f0b2756dd150422dc9ebe715a4104
IgorGabriel23 <igoorgns@gmail.com>
2024-04-28 15:08:10 -0300

    Added BootStrap

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,5 +9,5 @@ import { RouterOutlet } from '@angular/router';
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'bootcamp-devjr-modulo8-book';
+  title = 'Livros S/A';
 }
